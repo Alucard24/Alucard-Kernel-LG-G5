2fd971e0e0c98b51b4ac29b9749381bd560ba121
Yijing Wang <wangyijing@huawei.com>
2014-11-11 17:45:45 -0700

    PCI/MSI: Rename "struct msi_chip" to "struct msi_controller"
    "msi_chip" isn't very descriptive, so rename it to "msi_controller".  That
    tells a little more about what it does and is already used in device tree
    bindings.
    
    No functional change.
    
    [bhelgaas: changelog, change *only* the struct name so it's reviewable]
    Suggested-by: Bjorn Helgaas <bhelgaas@google.com>
    Signed-off-by: Yijing Wang <wangyijing@huawei.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    
    Git-commit: c2791b806988100cc1c047e2b0b5c5d0914aa3b6
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Signed-off-by: Puja Gupta <pujag@codeaurora.org>

--- include/linux/msi.h
+++ include/linux/msi.h
@@ -67,15 +67,15 @@ void default_restore_msi_irqs(struct pci_dev *dev);
 #define default_msi_mask_irq  __msi_mask_irq
 #define default_msix_mask_irq  __msix_mask_irq
 
-struct msi_chip {
+struct msi_controller {
 	struct module *owner;
 	struct device *dev;
 	struct device_node *of_node;
 	struct list_head list;
 
-	int (*setup_irq)(struct msi_chip *chip, struct pci_dev *dev,
+	int (*setup_irq)(struct msi_controller *chip, struct pci_dev *dev,
 			 struct msi_desc *desc);
-	void (*teardown_irq)(struct msi_chip *chip, unsigned int irq);
+	void (*teardown_irq)(struct msi_controller *chip, unsigned int irq);
 };
 
 #ifdef CONFIG_GENERIC_MSI_IRQ_DOMAIN
