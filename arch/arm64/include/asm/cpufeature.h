cfaaab6a7df0cd5957a431add4dcdde7001e1607
James Morse <james.morse@arm.com>
2015-07-21 13:23:28 +0100

    arm64: kernel: Add cpufeature 'enable' callback
    commit 1c0763037f1e1caef739e36e09c6d41ed7b61b2d upstream.
    
    This patch adds an 'enable()' callback to cpu capability/feature
    detection, allowing features that require some setup or configuration
    to get this opportunity once the feature has been detected.
    
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: James Morse <james.morse@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: David Brown <david.brown@linaro.org>

--- arch/arm64/include/asm/cpufeature.h
+++ arch/arm64/include/asm/cpufeature.h
@@ -35,6 +35,7 @@ struct arm64_cpu_capabilities {
 	const char *desc;
 	u16 capability;
 	bool (*matches)(const struct arm64_cpu_capabilities *);
+	void (*enable)(void);
 	union {
 		struct {	/* To be used for erratum handling only */
 			u32 midr_model;
