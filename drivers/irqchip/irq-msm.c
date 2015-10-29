22c4af266bff24ad4bee6e5d7ac5d37b3b5a9d64
Prasad Sodagudi <psodagud@codeaurora.org>
2015-10-29 23:16:11 +0530

    pinctrl: msm: add msmgold pinctrl driver
    Add initial pinctrl driver to support pin
    configuration with pinctrl framework for msmgold.
    
    Change-Id: I7bff01760eea276884312785bb12db3a85241726
    Signed-off-by: Prasad Sodagudi <psodagud@codeaurora.org>

--- drivers/irqchip/irq-msm.c
+++ drivers/irqchip/irq-msm.c
@@ -62,6 +62,7 @@ IRQCHIP_DECLARE(9640_pinctrl, "qcom,mdm9640-pinctrl", pinctrl_irq_dummy);
 IRQCHIP_DECLARE(fermium_pinctrl, "qcom,mdmfermium-pinctrl", pinctrl_irq_dummy);
 IRQCHIP_DECLARE(qpnp_irq, "qcom,spmi-pmic-arb", qpnpint_of_init);
 IRQCHIP_DECLARE(wcd9xxx_irq, "qcom,wcd9xxx-irq", wcd9xxx_irq_of_init);
+IRQCHIP_DECLARE(gold_pinctrl, "qcom,msmgold-pinctrl", pinctrl_irq_dummy);
 IRQCHIP_DECLARE(8952_pinctrl, "qcom,msm8952-pinctrl", pinctrl_irq_dummy);
 IRQCHIP_DECLARE(8937_pinctrl, "qcom,msm8937-pinctrl", pinctrl_irq_dummy);
 IRQCHIP_DECLARE(titanium_pinctrl, "qcom,msmtitanium-pinctrl",
