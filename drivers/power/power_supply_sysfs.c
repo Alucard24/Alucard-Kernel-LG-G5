d9d3de817cba12d61aecebbfbfd5bde71ea49c9e
Ashay Jaiswal <ashayj@codeaurora.org>
2015-12-11 17:50:26 +0530

    power_supply: Add TYPEC power supply properties
    Add TYPEC power supply type and following TYPEC related
    properties:
    
    - TYPE/TYPEC_MODE: property which is used to report the typec state
    	- TYPEC_UFP (upstream facing port).
    	- TYPEC_DFP (downstream facing port).
    - CURRENT_CAPABILITY: property which is used to report
    		current capability (in mA) of the charger.
    	This can be one of the following:
    	- 900, 1500, 3000
    
    Change-Id: I0eb838f11038eef139d3617e1e1e79b791173209
    Signed-off-by: Ashay Jaiswal <ashayj@codeaurora.org>
    
    Conflicts:
    	drivers/power/power_supply_sysfs.c
    	include/linux/power_supply.h

--- drivers/power/power_supply_sysfs.c
+++ drivers/power/power_supply_sysfs.c
@@ -69,7 +69,7 @@ static ssize_t power_supply_show_property(struct device *dev,
 		"Unknown", "Battery", "UPS", "Mains", "USB",
 		"USB_DCP", "USB_CDP", "USB_ACA",
 		"USB_HVDCP", "USB_HVDCP_3", "Wireless", "BMS", "USB_Parallel",
-		"Wipower"
+		"Wipower", "TYPEC", "TYPEC_UFP", "TYPEC_DFP"
 	};
 	static char *status_text[] = {
 		"Unknown", "Charging", "Discharging", "Not charging", "Full"
@@ -343,6 +343,8 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(cycle_count_id),
 	POWER_SUPPLY_ATTR(safety_timer_expired),
 	POWER_SUPPLY_ATTR(restricted_charging),
+	POWER_SUPPLY_ATTR(current_capability),
+	POWER_SUPPLY_ATTR(typec_mode),
 #ifdef CONFIG_LGE_PM_BATTERY_ID_CHECKER
 	POWER_SUPPLY_ATTR(batt_id),
 	POWER_SUPPLY_ATTR(valid_batt_id),
