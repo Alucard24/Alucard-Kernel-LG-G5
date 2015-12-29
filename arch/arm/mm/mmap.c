ff5e996b3680cf856e7d1da3701b7ee0e0897c4c
dcashman <dcashman@google.com>
2015-12-29 13:06:42 -0800

    Revert "arm: mm: support ARCH_MMAP_RND_BITS."
    This reverts commit bc8eb3ef20681a250f0e293044e3bb795f39e0cf.
    
    Bug: 25973686
    Signed-off-by: Daniel Cashman <dcashman@google.com>
    Change-Id: I568f9cb23c9b3ae1d66d35b1deb67a7a4eeaae3b

--- arch/arm/mm/mmap.c
+++ arch/arm/mm/mmap.c
@@ -183,8 +183,10 @@ void arch_pick_mmap_layout(struct mm_struct *mm)
 {
 	unsigned long random_factor = 0UL;
 
-	if (current->flags & PF_RANDOMIZE)
-		random_factor = arch_mmap_rnd();
+	/* 8 bits of randomness in 20 address space bits */
+	if ((current->flags & PF_RANDOMIZE) &&
+	    !(current->personality & ADDR_NO_RANDOMIZE))
+		random_factor = (get_random_int() % (1 << 8)) << PAGE_SHIFT;
 
 	if (mmap_is_legacy()) {
 		mm->mmap_base = TASK_UNMAPPED_BASE + random_factor;
