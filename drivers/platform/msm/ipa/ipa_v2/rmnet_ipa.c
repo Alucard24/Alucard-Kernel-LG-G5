a7c157bf59809c3da8b460c3599e9ac7c85b0119
Ghanim Fodi <gfodi@codeaurora.org>
2016-02-10 11:13:46 +0200

    msm: ipa3: Add IPA HAL layer I/S and registers support
    Add an IPA HAL layer I/S to IPA driver which abstracts
    the H/W towards the core IPA driver. Registers read/write
    access support is added.
    
    CRs-Fixed: 970323
    Change-Id: I9faf38798b53c263bd8f31d11061220b174f463d
    Signed-off-by: Ghanim Fodi <gfodi@codeaurora.org>

--- drivers/platform/msm/ipa/ipa_v2/rmnet_ipa.c
+++ drivers/platform/msm/ipa/ipa_v2/rmnet_ipa.c
@@ -1445,7 +1445,8 @@ static int ipa_wwan_ioctl(struct net_device *dev, struct ifreq *ifr, int cmd)
 					RMNET_IOCTL_EGRESS_FORMAT_CHECKSUM) {
 				apps_to_ipa_ep_cfg.ipa_ep_cfg.hdr.hdr_len = 8;
 				apps_to_ipa_ep_cfg.ipa_ep_cfg.cfg.
-					cs_offload_en = 1;
+					cs_offload_en =
+					IPA_ENABLE_CS_OFFLOAD_UL;
 				apps_to_ipa_ep_cfg.ipa_ep_cfg.cfg.
 					cs_metadata_hdr_offset = 1;
 			} else {
@@ -1504,7 +1505,8 @@ static int ipa_wwan_ioctl(struct net_device *dev, struct ifreq *ifr, int cmd)
 			if ((extend_ioctl_data.u.data) &
 					RMNET_IOCTL_INGRESS_FORMAT_CHECKSUM)
 				ipa_to_apps_ep_cfg.ipa_ep_cfg.cfg.
-					cs_offload_en = 2;
+					cs_offload_en =
+					IPA_ENABLE_CS_OFFLOAD_DL;
 
 			if ((extend_ioctl_data.u.data) &
 					RMNET_IOCTL_INGRESS_FORMAT_AGG_DATA) {
