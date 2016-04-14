4d8f1d841ea4bdc55fbd34764b6f7bc0fcc97e87
Phani Kumar Uppalapati <phaniu@codeaurora.org>
2016-04-14 13:34:36 -0700

    ASoC: wcd-mbhc: Fix issue with headset insert with extn.cable
    Fix continuous noise issue from headset when inserted/removed
    from other end of extension cable.
    
    CRs-fixed: 1003026
    Change-Id: Ic2560285e9a1776e86ca45ef875f121bb0b74a2d
    Signed-off-by: Phani Kumar Uppalapati <phaniu@codeaurora.org>

--- sound/soc/codecs/wcd-mbhc-v2.c
+++ sound/soc/codecs/wcd-mbhc-v2.c
@@ -682,7 +682,8 @@ static void wcd_mbhc_report_plug(struct wcd_mbhc *mbhc, int insertion,
 		if (mbhc->hph_status && mbhc->hph_status != jack_type) {
 #endif
 
-			if (mbhc->micbias_enable) {
+			if (mbhc->micbias_enable &&
+			    mbhc->current_plug == MBHC_PLUG_TYPE_HEADSET) {
 				if (mbhc->mbhc_cb->mbhc_micbias_control)
 					mbhc->mbhc_cb->mbhc_micbias_control(
 						mbhc->codec, MIC_BIAS_2,
