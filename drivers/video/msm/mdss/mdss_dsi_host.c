13842a5e95d84decfc317452ac99a697f535a7f9
Padmanabhan Komanduru <pkomandu@codeaurora.org>
2016-04-20 16:21:05 +0530

    msm: mdss: handle DSI error interrupts during dynamic fps
    During dynamic fps using PLL approach, it is expected that DSI
    FIFO underflow and DSI PLL unlock can happen sometimes. Make sure
    to mask these errors so that DSI error interrupt is not generated
    because of this. Clear the errors once dynamic refresh operation
    is done and unmask these errors later.
    
    Change-Id: I05ccbb7af1588b9ed81fd33ac14ef5b29882a42d
    Signed-off-by: Padmanabhan Komanduru <pkomandu@codeaurora.org>

--- drivers/video/msm/mdss/mdss_dsi_host.c
+++ drivers/video/msm/mdss/mdss_dsi_host.c
@@ -137,7 +137,7 @@ void mdss_dsi_ctrl_init(struct device *ctrl_dev,
 	}
 }
 
-static void mdss_dsi_set_reg(struct mdss_dsi_ctrl_pdata *ctrl, int off,
+void mdss_dsi_set_reg(struct mdss_dsi_ctrl_pdata *ctrl, int off,
 						u32 mask, u32 val)
 {
 	u32 data;
@@ -2960,7 +2960,7 @@ bool mdss_dsi_dln0_phy_err(struct mdss_dsi_ctrl_pdata *ctrl, bool print_en)
 
 static bool mdss_dsi_fifo_status(struct mdss_dsi_ctrl_pdata *ctrl)
 {
-	u32 status, isr;
+	u32 status;
 	unsigned char *base;
 	bool ret = false;
 
@@ -2972,17 +2972,7 @@ static bool mdss_dsi_fifo_status(struct mdss_dsi_ctrl_pdata *ctrl)
 	if (status & 0xcccc4409) {
 		MIPI_OUTP(base + 0x000c, status);
 
-		/*
-		 * When dynamic refresh operation is under progress, it is
-		 * expected to have FIFO underflow error sometimes. In such
-		 * cases, do not trigger the underflow recovery process and
-		 * avoid printing the error status on console.
-		 */
-		isr = MIPI_INP(ctrl->ctrl_base + 0x0110);
-		if (isr & DSI_INTR_DYNAMIC_REFRESH_MASK)
-			status &= ~(0x88880000);
-		else
-			pr_err("%s: status=%x\n", __func__, status);
+		pr_err("%s: status=%x\n", __func__, status);
 
 #if defined (CONFIG_LGE_MIPI_H1_INCELL_QHD_CMD_PANEL)
 		if(skip_lcd_error_check){
@@ -3036,6 +3026,10 @@ static bool mdss_dsi_clk_status(struct mdss_dsi_ctrl_pdata *ctrl)
 
 	if (status & 0x10000) { /* DSI_CLK_PLL_UNLOCKED */
 		MIPI_OUTP(base + 0x0120, status);
+		/* If PLL unlock is masked, do not report error */
+		if (MIPI_INP(base + 0x10c) & BIT(28))
+			return false;
+
 		dsi_send_events(ctrl, DSI_EV_PLL_UNLOCKED, 0);
 		pr_err("%s: status=%x\n", __func__, status);
 		ret = true;
