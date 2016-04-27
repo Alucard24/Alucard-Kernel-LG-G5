363048a18c16a8b4b5c3f5d3fe1cd359484e15bc
Chandana Kishori Chiluveru <cchiluve@codeaurora.org>
2016-04-27 19:10:36 +0530

    usb: android: Stop controller before disabling endpoints for USB2
    DWC3 controller requires stopping the controller only after removing
    the config that disables EPs as well. As this sequence is not recommended
    for chipidea USB2 controller, change it accordingly.
    
    Change-Id: I27f805fdf079710adf9149004bd3fc03aed91a88
    Signed-off-by: Chandana Kishori Chiluveru <cchiluve@codeaurora.org>
    
    Conflicts:
    	drivers/usb/gadget/android.c

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -662,19 +662,29 @@ static void android_disable(struct android_dev *dev)
 		if (cdev->gadget->evp_sts & (EVP_STS_EVP | EVP_STS_G_EN)) {
 			pr_info("%s: EVP cable is plugged, not permitted\n", __func__);
 		} else {
-			usb_gadget_disconnect(cdev->gadget);
-			dev->last_disconnect = ktime_get();
-		}
 #else
-		usb_gadget_disconnect(cdev->gadget);
-		dev->last_disconnect = ktime_get();
+			if (gadget_is_dwc3(cdev->gadget)) {
+				/* Cancel pending control requests */
+				usb_ep_dequeue(cdev->gadget->ep0, cdev->req);
+
+				list_for_each_entry(conf, &dev->configs, list_item)
+					usb_remove_config(cdev, &conf->usb_config);
+				usb_gadget_disconnect(cdev->gadget);
+				dev->last_disconnect = ktime_get();
+			} else {
+				usb_gadget_disconnect(cdev->gadget);
+				dev->last_disconnect = ktime_get();
+
+				/* Cancel pnding control requests */
+				usb_ep_dequeue(cdev->gadget->ep0, cdev->req);
+
+				list_for_each_entry(conf, &dev->configs, list_item)
+					usb_remove_config(cdev, &conf->usb_config);
+			}
 #endif
-
-		/* Cancel pending control requests */
-		usb_ep_dequeue(cdev->gadget->ep0, cdev->req);
-
-		list_for_each_entry(conf, &dev->configs, list_item)
-			usb_remove_config(cdev, &conf->usb_config);
+#ifdef CONFIG_LGE_USB_MAXIM_EVP
+		}
+#endif
 	}
 }
 
