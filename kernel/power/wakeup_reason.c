484023e0178ecdfec272719f9ee44468296196f8
Ruchi Kandoi <kandoiruchi@google.com>
2016-05-10 10:08:17 -0700

    power: Refactors the code which prints suspend time.
    Time for which device suspended is no longer tied to the persistent
    clocks. This is specially an issue for architecture which do not
    implement read_persistent_clocks()
    
    Bug: 22928771
    Change-Id: Ibbaec51819ddad8d38a169237390077b4307022d
    Signed-off-by: Ruchi Kandoi<kandoiruchi@google.com>

--- kernel/power/wakeup_reason.c
+++ kernel/power/wakeup_reason.c
@@ -26,6 +26,7 @@
 #include <linux/spinlock.h>
 #include <linux/notifier.h>
 #include <linux/suspend.h>
+#include <linux/debugfs.h>
 
 
 #define MAX_WAKEUP_REASON_IRQS 32
@@ -40,6 +41,12 @@ static ktime_t last_monotime; /* monotonic time before last suspend */
 static ktime_t curr_monotime; /* monotonic time after last suspend */
 static ktime_t last_stime; /* monotonic boottime offset before last suspend */
 static ktime_t curr_stime; /* monotonic boottime offset after last suspend */
+#if IS_ENABLED(CONFIG_SUSPEND_TIME)
+#ifdef CONFIG_SUSPEND_TIME_TIMEKEEPING
+static struct timespec suspend_time_before;
+#endif
+static unsigned int time_in_suspend_bins[32];
+#endif
 
 static ssize_t last_resume_reason_show(struct kobject *kobj, struct kobj_attribute *attr,
 		char *buf)
@@ -168,6 +175,15 @@ void log_suspend_abort_reason(const char *fmt, ...)
 static int wakeup_reason_pm_event(struct notifier_block *notifier,
 		unsigned long pm_event, void *unused)
 {
+#if IS_ENABLED(CONFIG_SUSPEND_TIME)
+#ifdef CONFIG_SUSPEND_TIME_TIMEKEEPING
+	struct timespec after;
+#else
+	ktime_t temp;
+	struct timespec suspend_time;
+#endif
+#endif
+
 	switch (pm_event) {
 	case PM_SUSPEND_PREPARE:
 		spin_lock(&resume_reason_lock);
@@ -178,12 +194,32 @@ static int wakeup_reason_pm_event(struct notifier_block *notifier,
 		last_monotime = ktime_get();
 		/* monotonic time since boot including the time spent in suspend */
 		last_stime = ktime_get_boottime();
+#ifdef CONFIG_SUSPEND_TIME_TIMEKEEPING
+		getnstimeofday(&suspend_time_before);
+#endif
 		break;
 	case PM_POST_SUSPEND:
 		/* monotonic time since boot */
 		curr_monotime = ktime_get();
 		/* monotonic time since boot including the time spent in suspend */
 		curr_stime = ktime_get_boottime();
+
+#if IS_ENABLED(CONFIG_SUSPEND_TIME)
+#ifdef CONFIG_SUSPEND_TIME_TIMEKEEPING
+		getnstimeofday(&after);
+		after = timespec_sub(after, suspend_time_before);
+		time_in_suspend_bins[fls(after.tv_sec)]++;
+		pr_info("Suspended for %lu.%03lu seconds\n", after.tv_sec,
+			after.tv_nsec / NSEC_PER_MSEC);
+#else
+		temp = ktime_sub(ktime_sub(curr_stime, last_stime),
+				ktime_sub(curr_monotime, last_monotime));
+		suspend_time = ktime_to_timespec(temp);
+		time_in_suspend_bins[fls(suspend_time.tv_sec)]++;
+		pr_info("Suspended for %lu.%03lu seconds\n", suspend_time.tv_sec,
+			suspend_time.tv_nsec / NSEC_PER_MSEC);
+#endif
+#endif
 		break;
 	default:
 		break;
@@ -194,6 +230,51 @@ static int wakeup_reason_pm_event(struct notifier_block *notifier,
 static struct notifier_block wakeup_reason_pm_notifier_block = {
 	.notifier_call = wakeup_reason_pm_event,
 };
+
+#if IS_ENABLED(CONFIG_DEBUG_FS) && IS_ENABLED(CONFIG_SUSPEND_TIME)
+static int suspend_time_debug_show(struct seq_file *s, void *data)
+{
+	int bin;
+	seq_printf(s, "time (secs)  count\n");
+	seq_printf(s, "------------------\n");
+	for (bin = 0; bin < 32; bin++) {
+		if (time_in_suspend_bins[bin] == 0)
+			continue;
+		seq_printf(s, "%4d - %4d %4u\n",
+			bin ? 1 << (bin - 1) : 0, 1 << bin,
+				time_in_suspend_bins[bin]);
+	}
+	return 0;
+}
+
+static int suspend_time_debug_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, suspend_time_debug_show, NULL);
+}
+
+static const struct file_operations suspend_time_debug_fops = {
+	.open		= suspend_time_debug_open,
+	.read		= seq_read,
+	.llseek		= seq_lseek,
+	.release	= single_release,
+};
+
+static int __init suspend_time_debug_init(void)
+{
+	struct dentry *d;
+
+	d = debugfs_create_file("suspend_time", 0755, NULL, NULL,
+		&suspend_time_debug_fops);
+	if (!d) {
+		pr_err("Failed to create suspend_time debug file\n");
+		return -ENOMEM;
+	}
+
+	return 0;
+}
+
+late_initcall(suspend_time_debug_init);
+#endif
 
 /* Initializes the sysfs parameter
  * registers the pm_event notifier
