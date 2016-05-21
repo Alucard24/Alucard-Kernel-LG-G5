181ccb09b95f9e137b93fbdcc20cd7af67017007
alucard24 <dmbaoh2@gmail.com>
2016-05-21 15:28:52 +0200

    Imported STATE_NOTIFIER!

--- drivers/input/touchscreen/lge/touch_core.c
+++ drivers/input/touchscreen/lge/touch_core.c
@@ -32,6 +32,10 @@
 #include <touch_core.h>
 #include <touch_common.h>
 
+#ifdef CONFIG_STATE_NOTIFIER
+#include <linux/state_notifier.h>
+#endif
+
 /* Gesture keycodes */
 #define KEY_GESTURE_SLIDE_UP	     249
 #define KEY_GESTURE_SLIDE_DOWN	     250
@@ -517,10 +521,17 @@ static int touch_fb_notifier_callback(struct notifier_block *self,
 	if (ev && ev->data && event == FB_EVENT_BLANK) {
 		int *blank = (int *)ev->data;
 
-		if (*blank == FB_BLANK_UNBLANK)
+		if (*blank == FB_BLANK_UNBLANK) {
 			touch_resume(ts->dev);
-		else if (*blank == FB_BLANK_POWERDOWN)
+#ifdef CONFIG_STATE_NOTIFIER
+			state_resume();
+#endif
+		} else if (*blank == FB_BLANK_POWERDOWN) {
 			touch_suspend(ts->dev);
+#ifdef CONFIG_STATE_NOTIFIER
+			state_suspend();
+#endif
+		}
 	}
 
 	return 0;
