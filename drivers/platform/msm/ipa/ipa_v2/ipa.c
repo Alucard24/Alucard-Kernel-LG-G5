arter97/caf_msm-3.10

msm: ipa: discard holb mitigation on Q6 pipes in ssr
When modem is shutdown, Q6 pipes are reset through uc.
As pipes are reset, holb mitigation logic kicks in
and ep_delay is set on all consumer pipes. At this point
if USB data transfer is going on and if USB is disconnected
IPA will not consume packets from USB and this results in
a forced crash. Make changes to discard holb monitoring on
Q6 pipes when modem is shutdown and enable back when QMI
handshake is completed. This will ensure holb monitoring
will not kick in when pipes are resetted.

Change-Id: Iaed23bb61b43eceef36c1fc5d264825d55e7268f
Acked-by: Chaitanya Pratapa <cpratapa@qti.qualcomm.com>
Signed-off-by: Ravinder Konka <rkonka@codeaurora.org>

drivers/platform/msm/ipa/ipa.c
@@ -1361,6 +1361,26 @@ static int ipa_q6_pipe_delay(bool zip_pipes)
 	return 0;
 }
 
+int ipa_q6_monitor_holb_mitigation(bool enable)
+{
+	int ep_idx;
+	int client_idx;
+
+	for (client_idx = 0; client_idx < IPA_CLIENT_MAX; client_idx++) {
+		if (IPA_CLIENT_IS_Q6_NON_ZIP_CONS(client_idx)) {
+			ep_idx = ipa_get_ep_mapping(client_idx);
+			if (ep_idx == -1)
+				continue;
+			/* Send a command to Uc to enable/disable
+			 * holb monitoring.
+			 */
+			ipa_uc_monitor_holb(client_idx, enable);
+		}
+	}
+
+	return 0;
+}
+
 static int ipa_q6_avoid_holb(bool zip_pipes)
 {
 	u32 reg_val;
@@ -1699,6 +1719,12 @@ int ipa_q6_pre_shutdown_cleanup(void)
 		IPAERR("Failed to delay Q6 pipes\n");
 		BUG();
 	}
+
+	if (ipa_q6_monitor_holb_mitigation(false)) {
+		IPAERR("Failed to disable HOLB monitroing on Q6 pipes\n");
+		BUG();
+	}
+
 	if (ipa_q6_avoid_holb(false)) {
 		IPAERR("Failed to set HOLB on Q6 pipes\n");
 		BUG();

Alucard24/Alucard-Kernel-LG-G5

drivers/platform/msm/ipa/ipa_v2/ipa.c
@@ -1365,6 +1365,26 @@ static int ipa_q6_pipe_delay(bool zip_pipes)
 	return 0;
 }
 
+int ipa_q6_monitor_holb_mitigation(bool enable)
+{
+	int ep_idx;
+	int client_idx;
+
+	for (client_idx = 0; client_idx < IPA_CLIENT_MAX; client_idx++) {
+		if (IPA_CLIENT_IS_Q6_NON_ZIP_CONS(client_idx)) {
+			ep_idx = ipa2_get_ep_mapping(client_idx);
+			if (ep_idx == -1)
+				continue;
+			/* Send a command to Uc to enable/disable
+			 * holb monitoring.
+			 */
+			ipa_uc_monitor_holb(client_idx, enable);
+		}
+	}
+
+	return 0;
+}
+
 static int ipa_q6_avoid_holb(bool zip_pipes)
 {
 	u32 reg_val;
@@ -1703,6 +1723,12 @@ int ipa_q6_pre_shutdown_cleanup(void)
 		IPAERR("Failed to delay Q6 pipes\n");
 		BUG();
 	}
+
+	if (ipa_q6_monitor_holb_mitigation(false)) {
+		IPAERR("Failed to disable HOLB monitroing on Q6 pipes\n");
+		BUG();
+	}
+
 	if (ipa_q6_avoid_holb(false)) {
 		IPAERR("Failed to set HOLB on Q6 pipes\n");
 		BUG();