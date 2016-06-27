d7f115e4b0549488c6bc8d442b3d860d0af2d7a1
Wei Wang <wvw@google.com>
2016-06-27 15:19:25 -0700

    msm-thermal: add option to disable core_control and disable it in 8996
    Bug: 29547080
    Change-Id: Id640c1dde041635423842328349a611df71679b6

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -6541,7 +6541,9 @@ static int probe_cc(struct device_node *node, struct msm_thermal_data *data,
 	int ret = 0;
 
 	if (num_possible_cpus() > 1) {
-		core_control_enabled = 1;
+		key = "qcom,disable-core-control";
+		if (!of_property_read_bool(node, key))
+			core_control_enabled = 1;
 		hotplug_enabled = 1;
 #ifdef CONFIG_LGE_PM_THERMAL_CTRL
 		core_control_force_mode = 0;
