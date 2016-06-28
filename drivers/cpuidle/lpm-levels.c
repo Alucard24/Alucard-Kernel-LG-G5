dcb1653e3e471df211a29e8429c20aa32a5e20de
alucard24 <dmbaoh2@gmail.com>
2016-06-28 20:39:14 +0200

    lpm-levels: Fixed .....
    [ 1424.777981 / 06-14 14:49:48.914][0] ------------[ cut here ]------------
    [ 1424.777981 / 06-14 14:49:48.914][0] WARNING: CPU: 0 PID: 1932 at kernel/time/timekeeping.c:539 ktime_get+0x30/0xa4()
    [ 1424.777981 / 06-14 14:49:48.914][0] Modules linked in: brcm_bt_drv fm_drv brcm_hci_ldisc
    [ 1424.777981 / 06-14 14:49:48.914][0] CPU: 0 PID: 1932 Comm: system_server Tainted: G        W      3.18.20-Alucard-PWR-CORE+ #1
    [ 1424.777981 / 06-14 14:49:48.914][0] Hardware name: Qualcomm Technologies, Inc. MSM 8996 v3.x + PMI8996 MTP (DT)
    [ 1424.777981 / 06-14 14:49:48.914][0] Call trace:
    [ 1424.777981 / 06-14 14:49:48.914][0] [<ffffffc000088848>] dump_backtrace+0x0/0xfc
    [ 1424.777981 / 06-14 14:49:48.914][0] [<ffffffc000088954>] show_stack+0x10/0x1c
    [ 1424.777981 / 06-14 14:49:48.914][0] [<ffffffc000ba9d2c>] dump_stack+0x80/0xc4
    [ 1424.777981 / 06-14 14:49:48.914][0] [<ffffffc0000a0958>] warn_slowpath_common+0x88/0xb0
    [ 1424.777981 / 06-14 14:49:48.914][0] [<ffffffc0000a0a44>] warn_slowpath_null+0x14/0x20
    [ 1424.777981 / 06-14 14:49:48.914][0] [<ffffffc0000fbf50>] ktime_get+0x2c/0xa4
    [ 1424.777981 / 06-14 14:49:48.914][0] [<ffffffc00080acc8>] lpm_suspend_enter+0x38/0x160
    [ 1424.777981 / 06-14 14:49:48.914][0] [<ffffffc0000e2284>] suspend_enter+0x2b4/0x508
    [ 1424.777981 / 06-14 14:49:48.914][0] [<ffffffc0000e25d0>] suspend_devices_and_enter+0xf8/0x27c
    [ 1424.777981 / 06-14 14:49:48.914][0] [<ffffffc0000e2ba8>] pm_suspend+0x454/0x4e8
    [ 1424.777981 / 06-14 14:49:48.914][0] [<ffffffc0000e11d0>] state_store+0x4c/0x88
    [ 1424.777981 / 06-14 14:49:48.914][0] [<ffffffc0002d401c>] kobj_attr_store+0x14/0x28
    [ 1424.777981 / 06-14 14:49:48.914][0] [<ffffffc0001ea80c>] sysfs_kf_write+0x40/0x4c
    [ 1424.777981 / 06-14 14:49:48.914][0] [<ffffffc0001e9b00>] kernfs_fop_write+0xfc/0x144
    [ 1424.777981 / 06-14 14:49:48.914][0] [<ffffffc00018be74>] vfs_write+0xb8/0x194
    [ 1424.777981 / 06-14 14:49:48.914][0] [<ffffffc00018c4b4>] SyS_write+0x44/0x84
    [ 1424.777981 / 06-14 14:49:48.914][0] ---[ end trace bb44740e361718fd ]---

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -851,15 +851,23 @@ static int lpm_cpuidle_enter(struct cpuidle_device *dev,
 	struct lpm_cluster *cluster = per_cpu(cpu_cluster, dev->cpu);
 	bool success = true;
 	const struct cpumask *cpumask = get_cpu_mask(dev->cpu);
-	int64_t start_time = ktime_to_ns(ktime_get()), end_time;
+	int64_t start_time, end_time;
 	struct power_params *pwr_params;
+
+	if (!timekeeping_suspended)
+		start_time = ktime_to_ns(ktime_get());
+	else
+		start_time = (~0ULL);
 
 	pwr_params = &cluster->cpu->levels[idx].pwr;
 	sched_set_cpu_cstate(smp_processor_id(), idx + 1,
 		pwr_params->energy_overhead, pwr_params->latency_us);
 
 	cpu_prepare(cluster, idx, true);
-	cluster_prepare(cluster, cpumask, idx, true, ktime_to_ns(ktime_get()));
+	if (!timekeeping_suspended)
+		cluster_prepare(cluster, cpumask, idx, true, ktime_to_ns(ktime_get()));
+	else
+		cluster_prepare(cluster, cpumask, idx, true, 0);
 
 	if (need_resched() || (idx < 0))
 		goto exit;
@@ -882,7 +890,11 @@ static int lpm_cpuidle_enter(struct cpuidle_device *dev,
 	}
 
 exit:
-	end_time = ktime_to_ns(ktime_get());
+	if (!timekeeping_suspended)
+		end_time = ktime_to_ns(ktime_get());
+	else
+		end_time = (~0ULL);
+
 	lpm_stats_cpu_exit(idx, end_time, success);
 
 	cluster_unprepare(cluster, cpumask, idx, true, end_time);
@@ -890,7 +902,11 @@ exit:
 
 	sched_set_cpu_cstate(smp_processor_id(), 0, 0, 0);
 	trace_cpu_idle_exit(idx, success);
-	end_time = ktime_to_ns(ktime_get()) - start_time;
+	if (!timekeeping_suspended)
+		end_time = ktime_to_ns(ktime_get()) - start_time;
+	else
+		end_time = (~0ULL);
+
 	dev->last_residency = do_div(end_time, 1000);
 	local_irq_enable();
 
@@ -1096,7 +1112,12 @@ static int lpm_suspend_enter(suspend_state_t state)
 	struct lpm_cpu *lpm_cpu = cluster->cpu;
 	const struct cpumask *cpumask = get_cpu_mask(cpu);
 	int idx;
-	int64_t time = ktime_to_ns(ktime_get());
+	int64_t time;
+
+	if (!timekeeping_suspended)
+		time = ktime_to_ns(ktime_get());
+	else
+		time = (~0ULL);
 
 	for (idx = lpm_cpu->nlevels - 1; idx >= 0; idx--) {
 
@@ -1130,7 +1151,11 @@ static int lpm_suspend_enter(suspend_state_t state)
 		update_debug_pc_event(CPU_EXIT, idx, true, 0xdeaffeed,
 					false);
 
-	time = ktime_to_ns(ktime_get());
+	if (!timekeeping_suspended)
+		time = ktime_to_ns(ktime_get());
+	else
+		time = (~0ULL);
+
 	cluster_unprepare(cluster, cpumask, idx, false, time);
 	cpu_unprepare(cluster, idx, false);
 	return 0;
