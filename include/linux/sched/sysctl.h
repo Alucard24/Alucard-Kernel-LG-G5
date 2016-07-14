d800b76236ad0ac6a3fa10559f5e96d8bd473d2e
Srinath Sridharan <srinathsr@google.com>
2016-07-14 09:57:29 +0100

    sched: EAS: take cstate into account when selecting idle core
    Introduce a new sysctl for this option, 'sched_cstate_aware'.
    When this is enabled, select_idle_sibling in CFS is modified to
    choose the idle CPU in the sibling group which has the lowest
    idle state index - idle state indexes are assumed to increase
    as sleep depth and hence wakeup latency increase. In this way,
    we attempt to minimise wakeup latency when an idle CPU is
    required.
    
    Signed-off-by: Srinath Sridharan <srinathsr@google.com>
    
    Includes:
    sched: EAS: fix select_idle_sibling
    
    when sysctl_sched_cstate_aware is enabled, best_idle cpu will not be chosen
    in the original flow because it will goto done directly
    
    Bug: 30107557
    Change-Id: Ie09c2e3960cafbb976f8d472747faefab3b4d6ac
    Signed-off-by: martin_liu <martin_liu@htc.com>
    
    Conflicts:
    	include/linux/sched/sysctl.h

--- include/linux/sched/sysctl.h
+++ include/linux/sched/sysctl.h
@@ -39,6 +39,7 @@ extern unsigned int sysctl_sched_latency;
 extern unsigned int sysctl_sched_min_granularity;
 extern unsigned int sysctl_sched_wakeup_granularity;
 extern unsigned int sysctl_sched_child_runs_first;
+extern unsigned int sysctl_sched_cstate_aware;
 extern unsigned int sysctl_sched_wake_to_idle;
 extern unsigned int sysctl_sched_wakeup_load_threshold;
 extern unsigned int sysctl_sched_window_stats_policy;
