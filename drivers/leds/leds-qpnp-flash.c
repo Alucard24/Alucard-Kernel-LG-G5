228e3320f0b12fc7d5f680a0730ebc928628658d
ansharma <ansharma@codeaurora.org>
2016-07-21 16:30:32 +0530

    ùdrivers: leds-qpnp-flash: Synchronize brightness_set and worker thread
    Synchronize between the brightness_set and worker thread by using
    led_access mutex lock from LED class device. This makes sure that
    the brightness level does not get changed when the register write
    is in progress and the last set brightness level always
    gets applied.
    
    CRs-Fixed: 1034167
    Change-Id: Ic15810b6a7fa1b2f658ce8415cd4767df29c9daa
    Signed-off-by: ansharma <ansharma@codeaurora.org>
    
    Conflicts:
    	drivers/leds/leds-qpnp-flash.c

--- drivers/leds/leds-qpnp-flash.c
+++ drivers/leds/leds-qpnp-flash.c
@@ -1207,7 +1207,7 @@ static void qpnp_flash_led_work(struct work_struct *work)
 	struct qpnp_flash_led *led =
 			dev_get_drvdata(&flash_node->spmi_dev->dev);
 	union power_supply_propval psy_prop;
-	int rc, brightness = flash_node->cdev.brightness;
+	int rc, brightness;
 	int max_curr_avail_ma = 0;
 	int total_curr_ma = 0;
 	int i;
@@ -1217,15 +1217,18 @@ static void qpnp_flash_led_work(struct work_struct *work)
 	u8 val;
 #endif
 
+	/* Global lock is to synchronize between the flash leds and torch */
 	mutex_lock(&led->flash_led_lock);
+	/* Local lock is to synchronize for one led instance */
+	mutex_lock(&flash_node->cdev.led_access);
 
+	brightness = flash_node->cdev.brightness;
 	if (!brightness)
 		goto turn_off;
 
 	if (led->open_fault) {
 		dev_err(&led->spmi_dev->dev, "Open fault detected\n");
-		mutex_unlock(&led->flash_led_lock);
-		return;
+		goto unlock_mutex;
 	}
 
 	if (!flash_node->flash_on && flash_node->num_regulators > 0) {
@@ -1235,10 +1238,8 @@ static void qpnp_flash_led_work(struct work_struct *work)
 #else
 		rc = flash_regulator_enable(led, flash_node, true);
 #endif
-		if (rc) {
-			mutex_unlock(&led->flash_led_lock);
-			return;
-		}
+		if (rc)
+			goto unlock_mutex;
 	}
 
 	if (!led->gpio_enabled && led->pinctrl) {
@@ -1731,6 +1732,8 @@ static void qpnp_flash_led_work(struct work_struct *work)
 	}
 
 	flash_node->flash_on = true;
+unlock_mutex:
+	mutex_unlock(&flash_node->cdev.led_access);
 	mutex_unlock(&led->flash_led_lock);
 
 	return;
@@ -1814,6 +1817,7 @@ error_enable_gpio:
 #endif
 
 	flash_node->flash_on = false;
+	mutex_unlock(&flash_node->cdev.led_access);
 	mutex_unlock(&led->flash_led_lock);
 
 	return;
