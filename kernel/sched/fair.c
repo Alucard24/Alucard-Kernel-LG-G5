ebb74ef1a4487cbc33094eefe41dfa3d888dae37
Patrick Bellasi <patrick.bellasi@arm.com>
2016-08-24 11:27:27 +0100

    FIXUP: sched/tune: update accouting before CPU capacity
    The SchedTune tasks accounting is used to identify how many tasks are in
    a boostgroup and thus to bias the selection of an OPP based on the
    maximum boost value of the active boostgroups.
    The current implementation however update the accounting after CPU
    capacity has been update. This has two effects:
    a) when we enqueue a boosted task, we do not immediately boost its CPU
    b) when we dequeue a boosted task, we can keep a CPU boosted even if not
       required
    
    This patch change the order of the SchedTune accounting and SchedFreq
    updated to ensure to have always an updated representation of which
    boosted tasks are runnable on a CPU before updating its capacity.
    
    Reported-by: Leo Yan <leo.yan@linaro.org>
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6086,6 +6086,25 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 
 #ifdef CONFIG_SMP
 
+	/*
+	 * Update SchedTune accounting.
+	 *
+	 * We do it before updating the CPU capacity to ensure the
+	 * boost value of the current task is accounted for in the
+	 * selection of the OPP.
+	 *
+	 * We do it also in the case where we enqueue a throttled task;
+	 * we could argue that a throttled task should not boost a CPU,
+	 * however:
+	 * a) properly implementing CPU boosting considering throttled
+	 *    tasks will increase a lot the complexity of the solution
+	 * b) it's not easy to quantify the benefits introduced by
+	 *    such a more complex solution.
+	 * Thus, for the time being we go for the simple solution and boost
+	 * also for throttled RQs.
+	 */
+	schedtune_enqueue_task(p, cpu_of(rq));
+
 	if (!se) {
 		if (!task_new && !rq->rd->overutilized &&
 		    cpu_overutilized(rq->cpu)) {
@@ -6104,9 +6123,6 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 		if (task_new || task_wakeup)
 			update_capacity_of(cpu_of(rq));
 	}
-
-	/* Update SchedTune accouting */
-	schedtune_enqueue_task(p, cpu_of(rq));
 
 #endif /* CONFIG_SMP */
 
@@ -6174,6 +6190,15 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 
 #ifdef CONFIG_SMP
 
+	/*
+	 * Update SchedTune accounting
+	 *
+	 * We do it before updating the CPU capacity to ensure the
+	 * boost value of the current task is accounted for in the
+	 * selection of the OPP.
+	 */
+	schedtune_dequeue_task(p, cpu_of(rq));
+
 	if (!se) {
 		dec_rq_hmp_stats(rq, p, 1);
 
@@ -6192,9 +6217,6 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 				set_cfs_cpu_capacity(cpu_of(rq), false, 0);
 		}
 	}
-
-	/* Update SchedTune accouting */
-	schedtune_dequeue_task(p, cpu_of(rq));
 
 #endif /* CONFIG_SMP */
 
