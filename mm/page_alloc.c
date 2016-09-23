d0d97528de6ad577bb064838d1f28227015f61b7
alucard24 <dmbaoh2@gmail.com>
2016-09-23 16:16:29 +0200

    Part of "Merge tag 'lsk-v3.18-16.04-android' into 'msm-3.18'"

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -788,6 +788,7 @@ static bool free_pages_prepare(struct page *page, unsigned int order)
 
 	trace_mm_page_free(page, order);
 	kmemcheck_free_shadow(page, order);
+	kasan_free_pages(page, order);
 
 	if (PageAnon(page))
 		page->mapping = NULL;
@@ -826,7 +827,7 @@ static void __free_pages_ok(struct page *page, unsigned int order)
 	local_irq_restore(flags);
 }
 
-void __free_pages_bootmem(struct page *page, unsigned long pfn,
+void  __free_pages_bootmem(struct page *page, unsigned long pfn,
 							unsigned int order)
 {
 	unsigned int nr_pages = 1 << order;
@@ -984,6 +985,7 @@ static int prep_new_page(struct page *page, unsigned int order, gfp_t gfp_flags)
 	kasan_alloc_pages(page, order);
 	arch_alloc_page(page, order);
 	kernel_map_pages(page, 1 << order, 1);
+	kasan_alloc_pages(page, order);
 
 	if (gfp_flags & __GFP_ZERO)
 		prep_zero_page(page, order, gfp_flags);
