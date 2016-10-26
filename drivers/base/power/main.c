070b712a495fd6c859f3f1d8beeac39e764c0a40
Siqi Lin <siqilin@google.com>
2016-10-26 11:25:42 -0700

    Revert "power: Add detailed suspend abort reason."
    This reverts commit a5db8e6805052318ea0ca17777c8cf376ca59427.
    
    Bug: 32371978
    Change-Id: I50db651d82385e7ee2b42a2a7527fbe7daffb68d
    Signed-off-by: Siqi Lin <siqilin@google.com>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -1226,15 +1226,11 @@ static int __device_suspend_noirq(struct device *dev, pm_message_t state, bool a
 	pm_callback_t callback = NULL;
 	char *info = NULL;
 	int error = 0;
-	char suspend_abort[MAX_SUSPEND_ABORT_LEN];
 
 	if (async_error)
 		goto Complete;
 
 	if (pm_wakeup_pending()) {
-		pm_get_active_wakeup_sources(suspend_abort,
-					 MAX_SUSPEND_ABORT_LEN);
-		log_suspend_abort_reason(suspend_abort);
 		async_error = -EBUSY;
 		goto Complete;
 	}
@@ -1264,13 +1260,11 @@ static int __device_suspend_noirq(struct device *dev, pm_message_t state, bool a
 	}
 
 	error = dpm_run_callback(callback, dev, state, info);
-	if (!error) {
+	if (!error)
 		dev->power.is_noirq_suspended = true;
-	} else {
-		log_suspend_abort_reason("Callback failed on %s in %pF returned %d",
-					 dev_name(dev), callback, error);
+	else
 		async_error = error;
-	}
+
 Complete:
 	complete_all(&dev->power.completion);
 	return error;
@@ -1371,7 +1365,6 @@ static int __device_suspend_late(struct device *dev, pm_message_t state, bool as
 	pm_callback_t callback = NULL;
 	char *info = NULL;
 	int error = 0;
-	char suspend_abort[MAX_SUSPEND_ABORT_LEN];
 
 	__pm_runtime_disable(dev, false);
 
@@ -1379,9 +1372,6 @@ static int __device_suspend_late(struct device *dev, pm_message_t state, bool as
 		goto Complete;
 
 	if (pm_wakeup_pending()) {
-		pm_get_active_wakeup_sources(suspend_abort,
-					 MAX_SUSPEND_ABORT_LEN);
-		log_suspend_abort_reason(suspend_abort);
 		async_error = -EBUSY;
 		goto Complete;
 	}
@@ -1411,13 +1401,10 @@ static int __device_suspend_late(struct device *dev, pm_message_t state, bool as
 	}
 
 	error = dpm_run_callback(callback, dev, state, info);
-	if (!error) {
+	if (!error)
 		dev->power.is_late_suspended = true;
-	} else {
-		log_suspend_abort_reason("Callback failed on %s in %pF returned %d",
-					 dev_name(dev), callback, error);
+	else
 		async_error = error;
-	}
 
 Complete:
 	complete_all(&dev->power.completion);
@@ -1587,7 +1574,7 @@ static int __device_suspend(struct device *dev, pm_message_t state, bool async)
 
 	if (dev->power.syscore)
 		goto Complete;
-
+	
 	data.dev = dev;
 	data.tsk = get_current();
 	init_timer_on_stack(&timer);
@@ -1670,9 +1657,6 @@ static int __device_suspend(struct device *dev, pm_message_t state, bool async)
 
 			spin_unlock_irq(&parent->power.lock);
 		}
-	} else {
-		log_suspend_abort_reason("Callback failed on %s in %pF returned %d",
-					 dev_name(dev), callback, error);
 	}
 
 	device_unlock(dev);
@@ -1873,9 +1857,6 @@ int dpm_prepare(pm_message_t state)
 			printk(KERN_INFO "PM: Device %s not prepared "
 				"for power transition: code %d\n",
 				dev_name(dev), error);
-			log_suspend_abort_reason("Device %s not prepared "
-						 "for power transition: code %d",
-						 dev_name(dev), error);
 			put_device(dev);
 			break;
 		}
