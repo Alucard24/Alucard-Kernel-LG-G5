962474f3c3ab1b8d5cd2c3ea68667abdc2ecae96
alucard24 <dmbaoh2@gmail.com>
2016-11-12 15:17:48 +0100

    Revert "Firmware broken"
    This reverts commit 6c32365b9218ffec8f655e410f59235a6c4c8f61.

--- cherryp.sh
+++
@@ -1,4 +0,0 @@
-git rev-list --reverse --topo-order 1a751d3574c3105b6de67c8946d4a92bc42920b0^..14b2a98123ed2538d163ca067935ee1bea572e3e | while read rev 
-do 
-  git cherry-pick $rev || break 
-done 
