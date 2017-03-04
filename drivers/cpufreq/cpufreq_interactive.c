98398ca7bfa619c4e17c798a05f0ce3820ce4430
Sultanxda <sultanxda@gmail.com>
2017-03-04 14:07:13 -0800

    cpufreq: interactive: Skip govinfo chain call when there are no listeners
    The only drivers that register CPUFREQ_GOVINFO_NOTIFIER notifiers are
    core_ctl and msm_performance, so if neither of those drivers are enabled,
    then the govinfo notifier chain call is unneeded. Compile it out entirely
    when this happens.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/cpufreq_interactive.c

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -594,7 +594,9 @@ static void __cpufreq_interactive_timer(unsigned long data, bool is_notif)
 	unsigned long flags;
 	unsigned long max_cpu;
 	int i, fcpu;
+#if defined(CONFIG_MSM_PERFORMANCE) || defined(CONFIG_SCHED_CORE_CTL)
 	struct cpufreq_govinfo govinfo;
+#endif
 
 	if (!down_read_trylock(&ppol->enable_sem))
 		return;
@@ -637,6 +639,7 @@ static void __cpufreq_interactive_timer(unsigned long data, bool is_notif)
 	}
 	spin_unlock_irqrestore(&ppol->load_lock, flags);
 
+#if defined(CONFIG_MSM_PERFORMANCE) || defined(CONFIG_SCHED_CORE_CTL)
 	/*
 	 * Send govinfo notification.
 	 * Govinfo notification could potentially wake up another thread
@@ -652,6 +655,7 @@ static void __cpufreq_interactive_timer(unsigned long data, bool is_notif)
 		atomic_notifier_call_chain(&cpufreq_govinfo_notifier_list,
 					   CPUFREQ_LOAD_CHANGE, &govinfo);
 	}
+#endif
 
 	spin_lock_irqsave(&ppol->target_freq_lock, flags);
 	cpu_load = loadadjfreq / ppol->target_freq;
